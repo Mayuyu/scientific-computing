17a963be99417a31b3b4509eaf39bb702e8a9ab4
马 征 <mazheng@Mz-MacBook-Pro.local>
2013-10-17 23:19:27 +0800

    Initial Commit

---
+++ scientific-computing/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  scientific-computing
+//
+//  Created by 马 征 on 13-10-17.
+//  Copyright (c) 2013年 马 征. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
