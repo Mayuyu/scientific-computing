e53c5ee6b667531bfee55a0459eab98cbac81c0c
马 征 <mazheng@Mz-MacBook-Pro.local>
2013-10-19 21:05:06 +0800

    Small change on the name of functions

--- scientific-computing/main.cpp
+++ scientific-computing/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 template <class T>
 T harmonic_F(T q, T a, T r, T r_s, T eta, T epsilon_o, T epsilon_i) {
-    double EPS=10e-4;
+    double EPS=10e-16;
     T x,R,t,sum=0.0,p3,p2=0.0,p1=1.0,e=1.0;
     int i=0;
     x=cos(eta);
@@ -41,21 +41,34 @@ dynamicVector<T> f(const dynamicVector<T> u, T r, T eta, T sigma) {
 
 template <class T>
 T image_F(T q, T a, T r, T r_s, T eta, T epsilon_o, T epsilon_i) {
-    T gamma, sigma, q_k, r_k, integral;
+    double EPS=10e-16;
+    T gamma, sigma, q_k, r_k, integral=0,tmp=0;
+    int i=100;
     gamma=(epsilon_i-epsilon_o)/(epsilon_i+epsilon_o);
     sigma=0.5*(1.0-gamma);
     r_k=a*a/r_s;
     q_k=-gamma*a/r_s;
-    dynamicVector<T> w(1000,0.0), x(1000,0.0);
-    gauleg(0.0, pow(r_k,sigma), x, w);
-    integral=w*f(x,r, eta, sigma);
+    do {
+        tmp=integral;
+        dynamicVector<T> w(i,0.0), x(i,0.0);
+        gauleg(0.0, pow(r_k,sigma), x, w);
+        integral=w*f(x,r, eta, sigma);
+        i++;
+    } while (abs(integral-tmp)>EPS);
     return q_k/(epsilon_o*sqrt(r*r+r_k*r_k-2.0*r*r_k*cos(eta)))+integral*gamma*pow(r_k, 1.0-sigma)/(epsilon_o*a);
 }
 
+double har_F(double r_a, double r_b, double eta) {
+    return harmonic_F(1.0, 1.0, r_a, r_b, eta, 1.0, 80.0);
+}
+
+double ima_F(double r_a, double r_b, double eta) {
+    return image_F(1.0, 1.0, r_a, r_b, eta, 1.0, 80.0);
+}
 int main(int argc, const char * argv[])
 {
-    cout<<harmonic_F(1.0, 1.0, 2.0, 2.0, 0.0, 1.0, 80.0)<<endl;
-    cout<<image_F(1.0, 1.0, 2.0, 2.0, 0.0, 1.0, 80.0)<<endl;
+    cout<<har_F(5.0, 10.0, M_PI/3)<<endl;
+    cout<<ima_F(5.0, 10.0, M_PI/3)<<endl;
     return 0;
 }
 
