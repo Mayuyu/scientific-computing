3c7b51bdd91029bba6cda391a178ca80d8f5e7da
马 征 <mazheng@Mz-MacBook-Pro.local>
2013-10-19 19:14:43 +0800

    Solve project 1.

--- scientific-computing/gauss_wgts.h
+++ scientific-computing/gauss_wgts.h
@@ -83,5 +83,60 @@ void gaulag(dynamicVector<T> &x, dynamicVector<T> &w, const T alf)
     }
 }
 
+template <class T>
+void gauher(dynamicVector<T> &x, dynamicVector<T> &w) //This routine returns arrays x[0..n-1] and w[0..n-1] containing the abscissas and weights of the n-point Gauss-Hermite quadrature formula. The largest abscissa is returned in x[0], the most negative in x[n-1].
+{
+    const double EPS=1.0e-14,PIM4=0.7511255444649425;
+    const int MAXIT=10;                             //Maximum iterations.
+    int i,its,j,m;
+    double p1,p2,p3,pp,z,z1;
+    int n=x.dim();
+    m=(n+1)/2;                              //The roots are symmetric about the origin, so we have to find only half of them.
+    for (i=0; i<m; i++) {
+        if (i==0) {
+             z=sqrt(double(2*n+1))-1.85575*pow(double(2*n+1),-0.16667);
+        } else if (i==1) {
+            z -= 1.14*pow(double(n),0.426)/z;
+        } else if (i==2) {
+            z=1.86*z-0.86*x[0];
+        } else if (i==3) {
+            z=1.91*z-0.91*x[1];
+        } else {
+            z=2.0*z-x[i-2];
+        }
+        for (its=0;its<MAXIT;its++) {
+            p1=PIM4;
+            p2=0.0;
+            for (j=0;j<n;j++) {
+                p3=p2;
+                p2=p1;
+                p1=z*sqrt(2.0/(j+1))*p2-sqrt(double(j)/(j+1))*p3;
+            }
+            pp=sqrt(double(2*n))*p2;
+            z1=z;
+            z=z1-p1/pp;                         //Newton’s formula.
+            if (abs(z-z1) <= EPS) break;
+        }
+        if (its >= MAXIT) throw("too many iterations in gauher");
+        x(i)=z;
+        x(n-1-i)=-z;
+        w(i)=2.0/(pp*pp);
+        w(n-1-i)=w[i];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif
--- scientific-computing/main.cpp
+++ scientific-computing/main.cpp
@@ -8,14 +8,54 @@
 
 #include <iostream>
 #include "gauss_wgts.h"
+#include <cmath>
 using namespace std;
+
+template <class T>
+T harmonic_F(T q, T a, T r, T r_s, T eta, T epsilon_o, T epsilon_i) {
+    double EPS=10e-4;
+    T x,R,t,sum=0.0,p3,p2=0.0,p1=1.0,e=1.0;
+    int i=0;
+    x=cos(eta);
+    t=R=(a*a)/(r*r_s);
+    while (abs(e)>EPS) {
+        p3=p2;
+        p2=p1;
+        p1=((2.0*i+1.0)*x*p2-i*p3)/(i+1);
+        t=R*t;
+        e=(t*(i+1)*(epsilon_o-epsilon_i)*p1)/((i+1)*epsilon_i+(i+2)*epsilon_o);
+        sum+=e;
+        i=i+1;
+    }
+    return (sum*q)/(a*epsilon_o);
+}
+
+template <class T>
+dynamicVector<T> f(const dynamicVector<T> u, T r, T eta, T sigma) {
+    dynamicVector<T> tmp(u.dim(),0.);
+    for (int i =0; i<u.dim(); i++) {
+        tmp(i)=1.0/sqrt(r*r+pow(u[i], 2.0/sigma)-2.0*r*pow(u[i], 1.0/sigma)*cos(eta));
+    }
+    return tmp;
+}
+
+template <class T>
+T image_F(T q, T a, T r, T r_s, T eta, T epsilon_o, T epsilon_i) {
+    T gamma, sigma, q_k, r_k, integral;
+    gamma=(epsilon_i-epsilon_o)/(epsilon_i+epsilon_o);
+    sigma=0.5*(1.0-gamma);
+    r_k=a*a/r_s;
+    q_k=-gamma*a/r_s;
+    dynamicVector<T> w(1000,0.0), x(1000,0.0);
+    gauleg(0.0, pow(r_k,sigma), x, w);
+    integral=w*f(x,r, eta, sigma);
+    return q_k/(epsilon_o*sqrt(r*r+r_k*r_k-2.0*r*r_k*cos(eta)))+integral*gamma*pow(r_k, 1.0-sigma)/(epsilon_o*a);
+}
 
 int main(int argc, const char * argv[])
 {
-    dynamicVector<double> a(4,0.),b(4,0.);
-    gaulag(a, b, 0.);
-    cout<<a<<" "<<endl;
-    cout<<b<<endl;
+    cout<<harmonic_F(1.0, 1.0, 2.0, 2.0, 0.0, 1.0, 80.0)<<endl;
+    cout<<image_F(1.0, 1.0, 2.0, 2.0, 0.0, 1.0, 80.0)<<endl;
     return 0;
 }
 
