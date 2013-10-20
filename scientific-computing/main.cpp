6d8aeb287203c8c3f56654e4482228ff4b4d6a57
马 征 <mazheng@Mz-MacBook-Pro.local>
2013-10-20 22:31:36 +0800

    Final version solving the problem by using  Gauss-Jacobi quadrature.

--- scientific-computing/main.cpp
+++ scientific-computing/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 template <class T>
 T harmonic_F(T q, T a, T r, T r_s, T eta, T epsilon_o, T epsilon_i) {
-    double EPS=10e-16;
+    double EPS=10e-14;
     T x,R,t,sum=0.0,p3,p2=0.0,p1=1.0,e=1.0;
     int i=0;
     x=cos(eta);
@@ -31,31 +31,34 @@ T harmonic_F(T q, T a, T r, T r_s, T eta, T epsilon_o, T epsilon_i) {
 }
 
 template <class T>
-dynamicVector<T> f(const dynamicVector<T> u, T r, T eta, T sigma) {
+dynamicVector<T> f(const dynamicVector<T> u, T r, T r_k, T eta) {
     dynamicVector<T> tmp(u.dim(),0.);
     for (int i =0; i<u.dim(); i++) {
-        tmp(i)=1.0/sqrt(r*r+pow(u[i], 2.0/sigma)-2.0*r*pow(u[i], 1.0/sigma)*cos(eta));
+        tmp(i)=1.0/sqrt(r*r+0.25*r_k*r_k*(1.0+u[i])*(1.0+u[i])-r_k*r*(1.0+u[i])*cos(eta));
     }
     return tmp;
 }
 
 template <class T>
 T image_F(T q, T a, T r, T r_s, T eta, T epsilon_o, T epsilon_i) {
-    double EPS=10e-16;
+    double EPS=10e-6;
     T gamma, sigma, q_k, r_k, integral=0,tmp=0;
-    int i=100;
+    int i=5;
     gamma=(epsilon_i-epsilon_o)/(epsilon_i+epsilon_o);
     sigma=0.5*(1.0-gamma);
     r_k=a*a/r_s;
     q_k=-gamma*a/r_s;
-    do {
-        tmp=integral;
-        dynamicVector<T> w(i,0.0), x(i,0.0);
-        gauleg(0.0, pow(r_k,sigma), x, w);
-        integral=w*f(x,r, eta, sigma);
-        i++;
-    } while (abs(integral-tmp)>EPS);
-    return q_k/(epsilon_o*sqrt(r*r+r_k*r_k-2.0*r*r_k*cos(eta)))+integral*gamma*pow(r_k, 1.0-sigma)/(epsilon_o*a);
+//    do {
+//        tmp=integral;
+//        dynamicVector<T> w(i,0.0), x(i,0.0);
+//        gaujac(x,w,0.0,sigma-1.0);
+//        integral=w*f(x,r,r_k,eta);
+//        i++;
+//    } while (abs(integral-tmp)>EPS);
+    dynamicVector<T> w(i,0.0), x(i,0.0);
+    gaujac(x,w,0.0,sigma-1.0);
+    integral=w*f(x,r,r_k,eta);
+    return q_k/(epsilon_o*sqrt(r*r+r_k*r_k-2.0*r*r_k*cos(eta)))+integral*gamma*sigma*r_k/(epsilon_o*a*pow(2.0, sigma));
 }
 
 double har_F(double r_a, double r_b, double eta) {
@@ -67,8 +70,8 @@ double ima_F(double r_a, double r_b, double eta) {
 }
 int main(int argc, const char * argv[])
 {
-    cout<<har_F(5.0, 10.0, M_PI/3)<<endl;
-    cout<<ima_F(5.0, 10.0, M_PI/3)<<endl;
+    cout<<har_F(2.0, 2.0, 0.0)<<endl;
+    cout<<ima_F(2.0, 2.0, 0.0)<<endl;
     return 0;
 }
 
